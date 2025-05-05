9871810b56204c5475e694e87971eab2f92fb708
Roxilius <fajrikhairan08@gmail.com>
2025-05-05 21:12:59 +0700

    .

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -189,7 +189,7 @@ const HomePage = () => {
           {/* Sidebar */}
           <div className={`w-full xl:w-4/12 flex flex-col gap-8 ${selectedGenre ? 'flex-col-reverse' : 'flex-col'}`}>
             {/* Top Airing */}
-            {/* <motion.div
+            <motion.div
               initial={{ opacity: 0, x: 50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5 }}
@@ -211,7 +211,7 @@ const HomePage = () => {
                   </motion.div>
                 ))}
               </div>
-            </motion.div> */}
+            </motion.div>
 
 
             {/* Genre List */}
